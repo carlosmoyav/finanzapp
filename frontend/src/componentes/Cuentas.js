ce24e277efa6eb99b48057a4de1740d91a09de33
carlosmoyav <carlosmoyaec@gmail.com>
2023-06-28 02:25:32 -0500

    frontend

--- frontend/src/componentes/Cuentas.js
+++ frontend/src/componentes/Cuentas.js
@@ -3,8 +3,13 @@ import React from 'react';
 function Cuentas(){
     return(
         <div className='contenedor-cuentas'>
+            <div className='contenedor-texto-cuentas'>
+                <p>hhh</p>
+            </div>
 
         </div>
 
     );
 }
+
+export default Cuentas;
