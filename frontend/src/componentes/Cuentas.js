3b34f8e6821a7c861b0e304fdd8cf9fd49dd09f2
carlosmoyav <carlosmoyaec@gmail.com>
2023-06-28 02:45:19 -0500

    cambios

--- frontend/src/componentes/Cuentas.js
+++ frontend/src/componentes/Cuentas.js
@@ -3,8 +3,14 @@ import React from 'react';
 function Cuentas(){
     return(
         <div className='contenedor-cuentas'>
-            <div className='contenedor-texto-cuentas'>
+            <div className='contenedor-tarjetas'>
                 <p>hhh</p>
+            </div>
+            <div className='contenedor-efectivo'>
+                <p>Efectivo</p>
+            </div>
+            <div className='contenedor-debitcard'>
+                <p>Tarjetas de credito</p>
             </div>
 
         </div>
